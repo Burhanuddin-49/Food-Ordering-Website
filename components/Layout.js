bbe5995bc15a9946820b4614c90e096ea26bd134
Burhanuddin-49 <burhanuddin9651@gmail.com>
2022-06-12 03:03:57 +0530

    fixed console error

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,27 @@
+import { useEffect, useState } from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 const Layout = ({ children }) => {
-  return (
-    <>
-      <Navbar />
-      {children}
-      <Footer />
-    </>
-  );
+  const [showChild, setShowChild] = useState(false);
+  useEffect(() => {
+    setShowChild(true);
+  }, []);
+
+  if (!showChild) {
+    return null;
+  }
+  if (typeof window === "undefined") {
+    return <></>;
+  } else {
+    return (
+      <>
+        <Navbar />
+        {children}
+        <Footer />
+      </>
+    );
+  }
 };
 
 export default Layout;
